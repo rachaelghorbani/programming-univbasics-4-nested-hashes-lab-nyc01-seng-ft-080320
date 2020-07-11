481f10ee18c00293a36af0baa505fe6f63b0c89d
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-11 17:33:57 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +55,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+
+programmer_hash[:dennis_ritchie][:languages][0]
+
 
 end
 
@@ -81,7 +84,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
+programmer_hash
 end
 
 def changing_alan
@@ -103,6 +110,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    alans_new_info = "GUI"
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
 
 end
 
@@ -125,5 +135,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 end
