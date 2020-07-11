3dc5931479d2c15649922e3c3323ffaf47901b39
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-11 17:17:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
       }
     }
 
-programmer_hash[:dennis_ritchie][:languages]
+language = programmer_hash[:dennis_ritchie][:languages]
+language = language.to_s
 end
 
 def adding_matz
